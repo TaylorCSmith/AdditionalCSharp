56f3f4f2a9e88539df8d074e22cb4e5cf99d924a
Taylor Smith <tcs140230@utdallas.edu>
2017-08-15 16:11:23 -0500

    initial commit, have made color palette and base game loop

---
+++ RogueLikeGame/README.txt
@@ -0,0 +1,13 @@
+INTRODUCTION: 
+
+This is meant to just be a side project to take a break from more "serious" things I'm learning/working on at the time. I used to play "roguelike" games a lot so I thought that I might as well try and make my own. I always found game development to be a sort of interesting way of implementing programming concepts so I wanted to sort of explore it by... well... making a game! Below is the general brainstorming for the game. It'll probably look like a bunch of jargon but it's just what I type out while I'm thinking of ideas for it. 
+
+If you want to get involved in this for whatever reason... email me at tcs140230@utdallas.edu
+
+
+ROGUELIKE GAME: 
+
+Greg the Barbarian
+Different Hats for Different Classes
+
+Overall goal: get the bag of lost hats. 
---
+++ RogueLikeGame/RogueLikeGame/Core/Palette.cs
@@ -0,0 +1,54 @@
+﻿using RLNET;
+
+namespace RogueLikeGame.Core
+{
+    class Palette
+    {
+        // here's the color pallete I used:
+        // http://paletton.com/#uid=7240u0k5qgb2NnT41jT74c8bJ8X
+
+        public static RLColor PrimaryLightest = new RLColor(183, 185, 169);
+        public static RLColor PrimaryLighter = new RLColor(151, 154, 135);
+        public static RLColor Primary = new RLColor(122, 125, 104);
+        public static RLColor PrimaryDarker = new RLColor(91, 94, 73);
+        public static RLColor PrimaryDarkest = new RLColor(66, 69, 44);
+
+        public static RLColor SecondaryLightest = new RLColor(164,171,157);
+        public static RLColor SecondaryLighter = new RLColor(134,143,125);
+        public static RLColor Secondary = new RLColor(106,116,97);
+        public static RLColor SecondaryDarker = new RLColor(78,87,68);
+        public static RLColor SecondaryDarkest = new RLColor(53,64,41);
+
+        public static RLColor AlternateLightest = new RLColor(167,152,157);
+        public static RLColor AlternateLighter = new RLColor(139,121,128);
+        public static RLColor Alternate = new RLColor(113,94,101);
+        public static RLColor AlternateDarker = new RLColor(85,66,73);
+        public static RLColor AlternateDarkest = new RLColor(62,40,48);
+
+        public static RLColor ComplimentLightest = new RLColor(124,114,125);
+        public static RLColor ComplimentLighter = new RLColor(103,91,104);
+        public static RLColor Compliment = new RLColor(83,70,84);
+        public static RLColor ComplimentDarker = new RLColor(62,50,63);
+        public static RLColor ComplimentDarkest = new RLColor(45,30,47);
+
+        // from tutorial
+        // http://pixeljoint.com/forum/forum_posts.asp?TID=12795
+
+        public static RLColor DbDark = new RLColor(20, 12, 28);
+        public static RLColor DbOldBlood = new RLColor(68, 36, 52);
+        public static RLColor DbDeepWater = new RLColor(48, 52, 109);
+        public static RLColor DbOldStone = new RLColor(78, 74, 78);
+        public static RLColor DbWood = new RLColor(133, 76, 48);
+        public static RLColor DbVegetation = new RLColor(52, 101, 36);
+        public static RLColor DbBlood = new RLColor(208, 70, 72);
+        public static RLColor DbStone = new RLColor(117, 113, 97);
+        public static RLColor DbWater = new RLColor(89, 125, 206);
+        public static RLColor DbBrightWood = new RLColor(210, 125, 44);
+        public static RLColor DbMetal = new RLColor(133, 149, 161);
+        public static RLColor DbGrass = new RLColor(109, 170, 44);
+        public static RLColor DbSkin = new RLColor(210, 170, 153);
+        public static RLColor DbSky = new RLColor(109, 194, 202);
+        public static RLColor DbSun = new RLColor(218, 212, 94);
+        public static RLColor DbLight = new RLColor(222, 238, 214);
+    }
+}
