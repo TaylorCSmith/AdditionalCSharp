816f68c1e766b8167eb6d4a90d8fdb33cc736fcd
Taylor Smith <tcs140230@utdallas.edu>
2017-09-20 20:39:44 -0500

    fixing problem

---
+++ AceOfSpacesPizza/AceOfSpadesPizza/Controllers/OrderController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AceOfSpadesPizza.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult PlaceOrder()
+        {
+            return View();
+        }
+
+        public ActionResult ViewOrders()
+        {
+            return View();
+        }
+    }
+}
---
+++ AceOfSpacesPizza/AceOfSpadesPizza/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AceOfSpadesPizza.Models
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public int PhoneNumber { get; set; }
+        public DateTime DeliveryDate { get; set; }
+        public int PizzaCount { get; set; }
+    }
+}
