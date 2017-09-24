873d70f75995f11f66f25ff900607e1393969922
Taylor Smith <tcs140230@utdallas.edu>
2017-09-24 13:15:20 -0500

    Finished shopping cart

---
+++ MVCBookWork/Ch8_SportsStoreNavigation/SportsStore/SportsStore.WebUI/Controllers/CartController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Models;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private IProductsRepository repository;
+
+        public CartController(IProductsRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = GetCart(),
+                ReturnUrl = returnUrl
+            });
+        }
+
+        // RedirectToRouteResult sends the HTTP redirect instruction to the client browser... 
+        // this causes the browser to request a new URL
+        public RedirectToRouteResult AddToCart(int productId, string returnUrl)
+        {
+            Product product = repository.Products
+                .FirstOrDefault(p => p.ProductID == productId);
+
+            if (product != null)
+            {
+                GetCart().AddItem(product, 1);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToRouteResult RemoveFromCart(int productId, string returnUrl)
+        {
+            Product product = repository.Products
+                .FirstOrDefault(p => p.ProductID == productId);
+
+            if (product != null)
+            {
+                GetCart().RemoveLine(product);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        private Cart GetCart()
+        {
+            Cart cart = (Cart)Session["Cart"];  // refers to browser section. used to store and retrieve cart objects
+            if(cart == null)                    // Data association with a session is deleted when when a session expires (normally because a user has not made a request for a while)
+            {                                   // pairs the cart with a user by generating a unique key, the cart is retrieved by using that key. Similar to a Dictionary DS
+                cart = new Cart();              // Session state objects are stored in the memory of the ASP.NET server by default
+                Session["Cart"] = cart;
+            }
+            return cart;
+        }
+    }
+}
